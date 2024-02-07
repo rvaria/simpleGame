2976e8373ba76406599d396f26812c2942a3d38e
Riddhi <ZHAC110@live.rhul.ac.uk>
2024-02-07 15:33:30 +0000

    Implement simple jump mechanic

--- simpleGame/Player.cs
+++ simpleGame/Player.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using System.Runtime.InteropServices;
 using System;
 
 namespace simpleGame
@@ -12,11 +11,15 @@ namespace simpleGame
         private Texture2D player;
         private Vector2 pos;
         private KeyboardState key;
+        private const int walk = 1;
+        private const int jump = 20;
+        private const int gravity = 10;
+        private Boolean isJumping = false;
 
         public Player(ContentManager content) {
             
             player = content.Load<Texture2D>("ball");
-            pos = new Vector2(100, 100);
+            pos = new Vector2(200, 200);
     }
 
         public void draw(SpriteBatch spriteBatch)
@@ -27,19 +30,29 @@ namespace simpleGame
 
         public void keyPress()
         {
+
             key = Keyboard.GetState();
 
             if (key.IsKeyDown(Keys.Up) || key.IsKeyDown(Keys.W)) {
-                pos.Y -= 1;
-            } else if (key.IsKeyDown(Keys.Left) || key.IsKeyDown(Keys.A))
+                pos.Y -= jump;
+                isJumping = true;
+
+            }
+            else if (key.IsKeyDown(Keys.Left) || key.IsKeyDown(Keys.A))
             {
-                pos.X -= 1;
+                pos.X -= walk;
             } else if (key.IsKeyDown(Keys.Right) || key.IsKeyDown(Keys.D))
             {
-                pos.X += 1;
+                pos.X += walk;
             } else if (key.IsKeyDown(Keys.Down) || key.IsKeyDown(Keys.S))
             {
-                pos.Y += 1;
+                pos.Y += walk;
+            }
+
+            if (isJumping)
+            {
+                pos.Y += gravity;
+
             }
         }
     }
